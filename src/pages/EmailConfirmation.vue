55d7b06d78ca05c159482f8437e73ab694b98c1a
Anatalia Neves Mula <mulaanatalia@gmail.com>
2023-03-05 23:32:29 +0200

    criação das páginas de registo e login

---
+++ src/pages/EmailConfirmation.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <p class="text-body1">
+      Obrigado por se registar!
+    </p>
+    <p class="text-body1">
+      Por favor confirme o seu email para terminar o registo: <strong> {{ $route.query.email }} </strong>
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation'
+})
+</script>
